7245fdc9cb797328e2fc847c2a15c5738b08f15d
chesbroughc <98974955+chesbroughc@users.noreply.github.com>
2023-08-11 19:03:47 +1000

    Add files via upload
    Learning to use foreach loops to iterate through arrays.

---
+++ MicrosoftLearnC#-Part2ArraysAndForEachLoops.cs
@@ -0,0 +1,18 @@
+﻿//string[] names = {"Rowena", "Robins", "Bao"};
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
+
+int[] inventory = {200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+
+foreach (int i in inventory)
+{
+    sum += i;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {i} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
